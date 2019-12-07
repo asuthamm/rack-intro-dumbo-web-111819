0769da9f05070981f03a7869c1e0d1f63fc1a649
asuthamm <asuthamm@gmail.com>
2019-12-07 15:02:59 -0500

    Done.

--- application.rb
+++ application.rb
@@ -1,8 +1,9 @@
+# application.rb
 class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is"
     resp.finish
   end
 
---
+++ my_server.rb
@@ -0,0 +1,12 @@
+class MyServer
+    def call(env)
+      return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
+    end
+   
+    def pretty_response
+      (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hey, Hello</strong>"]
+    end
+  end
+
+
+  [200, {"Content-Type" => "text/html"}, ["Hello <em>World</em>!"]]
